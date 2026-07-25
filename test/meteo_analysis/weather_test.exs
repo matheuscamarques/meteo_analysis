7d74b77ac3fdc45ac044b52405d33d797c40c4b4
Matheus C Marques <matheuscamarques@gmail.com>
2026-07-25 14:44:39 -0300

    test(weather): add test for concurrent processing of cities [RED]

---
+++ test/meteo_analysis/weather_test.exs
@@ -0,0 +1,35 @@
+defmodule MeteoAnalysis.WeatherTest do
+  use ExUnit.Case, async: true
+  import Mox
+
+  alias MeteoAnalysis.{City, Weather, ClientMock}
+
+  setup :verify_on_exit!
+
+  describe "process_cities/2" do
+    test "processa concorrentemente todas as cidades e calcula a temperatura máxima média" do
+      sp = %City{name: "São Paulo", latitude: -23.55, longitude: -46.63}
+      bh = %City{name: "Belo Horizonte", latitude: -19.92, longitude: -43.94}
+      cur = %City{name: "Curitiba", latitude: -25.43, longitude: -49.27}
+
+      expect(ClientMock, :fetch_forecast, fn ^sp ->
+        {:ok, [28.5, 29.3, 27.1, 26.8, 29.0, 30.3]}
+      end)
+
+      expect(ClientMock, :fetch_forecast, fn ^bh ->
+        {:ok, [27.0, 28.0, 27.5, 28.2, 27.8, 28.3]}
+      end)
+
+      expect(ClientMock, :fetch_forecast, fn ^cur ->
+        {:ok, [21.5, 22.0, 22.5, 21.8, 22.2, 22.6]}
+      end)
+
+      results = Weather.process_cities([sp, bh, cur], ClientMock)
+
+      assert length(results) == 3
+      assert {:ok, "São Paulo", 28.5} in results
+      assert {:ok, "Belo Horizonte", 27.8} in results
+      assert {:ok, "Curitiba", 22.1} in results
+    end
+  end
+end
